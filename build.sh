d1f5613a047eee72271c182b6b5728e4e257ce5e
Michele Masili <michele.masili@creactives.com>
2021-07-10 19:46:03 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,29 @@
-if [ "$GOOS" = "windows" ]; then
-  go build -o osm.exe cmd/cli/main.go
+#!/bin/bash
+export GOPATH=$HOME/go
+if [ "${GITHUB_ACTIONS}" == "true" ]; then
+go test 1> debug.out
 else
-  go build -o osm cmd/cli/main.go
+go test -v
+fi
+
+go get 1>> debug.out
+
+BINARY="osm"
+
+if [ $? == 0 ]; then
+  if [ "${GOOS}" == "windows" ]; then
+    if [ "${GITHUB_ACTIONS}" == "true" ]; then
+go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o ${BINARY}.exe cmd/cli/main.go 1>> debug.out
+echo "${BINARY}.exe"
+    else
+go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o ${BINARY}.exe cmd/cli/main.go
+    fi
+  else
+    if [ "${GITHUB_ACTIONS}" == "true" ]; then
+go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o ${BINARY} cmd/cli/main.go 1>> debug.out
+echo "${BINARY}"
+    else
+go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o ${BINARY} cmd/cli/main.go
+    fi
+  fi
 fi
